6a1bc30d40d7690745be4d5fa469576a4d56e323
Siegfred <dante_sparda1925@yahoo.com>
2021-09-24 08:27:15 +0300

    added codes

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -10,7 +10,6 @@ import {FaGlobe,FaLaptopCode } from "react-icons/fa"
 const Portfolio = () => {
     return (
         <div className="portfolio-page-container">
-          
             <div className="page-heading-container text-center py-5 ">
             <Fade top>
                 <div className="page-heading-title">
@@ -23,106 +22,102 @@ const Portfolio = () => {
                         Check out some of my personal projects.
                     </h5>
                 </div>
-                </Fade>
+            </Fade>
             </div>
-            
             <Fade >
             <div className="container portfolio-projects-container">
-            <div className="container py-5">
-            <div className="row row-cols-1 row-cols-md-3 g-4">
-    <div className="col">
-    <div className="card h-100">
-      <img src={MovieSearchImage}className="card-img-top" alt="..."/>
-      <div className="card-body">
-        <h5 className="card-title text-center fw-bold">Movie Search App- Hooked</h5>
-        <p className="card-text text-primary">Technologies Used:React, React Router, Bootstrap, HTML, CSS, REST API</p>
-        <h6 card-title>The Brief</h6>
-        <p className="card-text">
-          The Covid-19 pandemic has changed the way we live.People tries to think of different ways to make the time they spend at home as enjoyable as possible.More time at home means more time for streaming movies and watching TV.
-        </p>
-        <h6 card-title>The Result</h6>
-        <p className="card-text">
-          Hooked is a movie search app that lets you do basic search of a particular movie.It returns basic information and overview of the movie.It also provides an updated list of trending movie recommendation.Hooked is responsive and can be viewed in different devices.
-        </p>
-      </div>
-      <div className="project-links text-center p-2">
-        <button className="btn btn-primary rounded-pill"><a href="https://github.com/iamtigermaximus/hooked2021" target="_blank" rel="noopener noreferrer" className="button-link"><FaLaptopCode/></a></button>
-        <button className="btn btn-primary rounded-pill"><a href="https://tigercode-hooked2021.netlify.app/" target="_blank" rel="noopener noreferrer" className="button-link"><FaGlobe/></a></button>
-      </div>
-    </div>
-  </div>
-  <div className="col">
-    <div className="card h-100">
-      <img src={CovidAppImage} className="card-img-top" alt="..."/>
-      <div className="card-body">
-        <h5 className="card-title text-center fw-bold">Corona Tracker</h5>
-        <p className="card-text text-primary">Technologies Used:React, React Router, Bootstrap, HTML, CSS, REST API</p>
-        <h6 card-title>The Brief</h6>
-        <p className="card-text">
-        Covid Tracker apps are everywhere, but my biggest issue with them all is how bloated they all are. Social login, user tracking, email notifications. The list goes on. I wanted something really simple but shows the important data needed by the user.
-        </p>
-        <h6 card-title>The Result</h6>
-        <p className="card-text">
-         Corona Tracker is a prevalence app that provides up-to-date cases of Covid-19 around the world.It provides a dashboard that returns number of cases, deaths and recovered.The app also can give information about a country's status as well as travel advisories.Corona Tracker is responsive and can be viewed in different devices.
-        </p>
-      </div>
-      <div className="project-links text-center p-2">
-        <button className="btn btn-primary rounded-pill"><a href="https://github.com/iamtigermaximus/coronatracker2021" target="_blank" rel="noopener noreferrer" className="button-link"><FaLaptopCode/></a></button>
-        <button className="btn btn-primary rounded-pill"><a href="https://tigercode-coronatracker2021.netlify.app/" target="_blank"  rel="noopener noreferrer" className="button-link"><FaGlobe/></a></button>
-      </div>
-    </div>
-  </div>
-  <div className="col">
-    <div className="card h-100">
-      <img src={RestaurantImage} className="card-img-top" alt="..."/>
-      <div className="card-body">
-        <h5 className="card-title text-center fw-bold">Restaurant Website- Moma</h5>
-        <p className="card-text text-primary">Technologies Used:React, React Router, Bootstrap, HTML, CSS</p>
-        <h6 card-title>The Brief</h6>
-        <p className="card-text">
-         A website gives a restaurant business a platform to provide the basic information the customers may need to know.It gives potential customers a feel for your restaurant, menu and atmosphere.
-        </p>
-        <h6 card-title>The Result</h6>
-        <p className="card-text">
-          Moma provides basic information about the restaurant's menu,the kind of service, how to book and reserve a table, contact information and a blog page.Moma is responsive and can be viewed in different devices. 
-        </p>
-      </div>
-      <div className="project-links text-center p-2">
-        <button className="btn btn-primary rounded-pill"><a href="https://github.com/iamtigermaximus/restaurant-website" target="_blank" rel="noopener noreferrer" className="button-link"><FaLaptopCode/></a></button>
-        <button className="btn btn-primary rounded-pill"><a href="https://tigercode-restaurant-website.netlify.app/" target="_blank"  rel="noopener noreferrer" className="button-link"><FaGlobe/></a></button>
-      </div>
-    </div>
-  </div>
-  <div className="col">
-    <div className="card h-100">
-      <img src={PortfolioImage} className="card-img-top" alt="..."/>
-      <div className="card-body">
-        <h5 className="card-title text-center fw-bold">Portfolio Website</h5>
-        <p className="card-text text-primary">Technologies Used:React, React Router, Bootstrap, HTML, CSS</p>
-        <h6 card-title>The Brief</h6>
-        <p className="card-text">
-          There are certain careers where portfolios prove more helpful than just resume in showcasing your skills and experience.As a self-taught developer, I believe that portfolio websites will help employers assess whether the skills and knowledge of a potential employee fits their needs.
-        </p>
-        <h6 card-title>The Result</h6>
-        <p className="card-text">
-          I built a portfolio website to showcase a collection of my personal projects.It also provides basic information about myself, contact details and links to project websites.The portfolio website is responsive and can be viewed in different devices.
-        </p>
-      </div>
-      <div className="project-links text-center p-2">
-        <button className="btn btn-primary rounded-pill"><a href="https://github.com/iamtigermaximus/2021portfolio2021" target="_blank" rel="noopener noreferrer" className="button-link"><FaLaptopCode/></a></button>
-        <button className="btn btn-primary rounded-pill"><a href="https://tigerportfolio2021.netlify.app/" target="_blank" rel="noopener noreferrer" className="button-link"><FaGlobe/></a></button>
-      </div>
-    </div>
-  </div>
-  
-  
-</div>
+              <div className="container py-5">
+                <div className="row row-cols-1 row-cols-md-3 g-4">
+                  <div className="col">
+                    <div className="card h-100">
+                      <img src={MovieSearchImage}className="card-img-top" alt="..."/>
+                     <div className="card-body">
+                        <h5 className="card-title text-center fw-bold">Movie Search App- Hooked</h5>
+                        <p className="card-text text-primary">Technologies Used:React, React Router, Bootstrap, HTML, CSS, REST API</p>
+                        <h6 card-title>The Brief</h6>
+                        <p className="card-text">
+                          The Covid-19 pandemic has changed the way we live.People tries to think of different ways to make the time they spend at home as enjoyable as possible.More time at home means more time for streaming movies and watching TV.
+                        </p>
+                        <h6 card-title>The Result</h6>
+                        <p className="card-text">
+                          Hooked is a movie search app that lets you do basic search of a particular movie.It returns basic information and overview of the movie.It also provides an updated list of trending movie recommendation.Hooked is responsive and can be viewed in different devices.
+                        </p>
+                      </div>
+                    <div className="project-links text-center p-2">
+                      <button className="btn btn-primary rounded-pill"><a href="https://github.com/iamtigermaximus/hooked2021" target="_blank" rel="noopener noreferrer" className="button-link"><FaLaptopCode/></a></button>
+                      <button className="btn btn-primary rounded-pill"><a href="https://tigercode-hooked2021.netlify.app/" target="_blank" rel="noopener noreferrer" className="button-link"><FaGlobe/></a></button>
+                    </div>
+                  </div>
+                </div>
+              <div className="col">
+                <div className="card h-100">
+                  <img src={CovidAppImage} className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                    <h5 className="card-title text-center fw-bold">Corona Tracker</h5>
+                    <p className="card-text text-primary">Technologies Used:React, React Router, Bootstrap, HTML, CSS, REST API</p>
+                    <h6 card-title>The Brief</h6>
+                    <p className="card-text">
+                      Covid Tracker apps are everywhere, but my biggest issue with them all is how bloated they all are. Social login, user tracking, email notifications. The list goes on. I wanted something really simple but shows the important data needed by the user.
+                    </p>
+                    <h6 card-title>The Result</h6>
+                    <p className="card-text">
+                      Corona Tracker is a prevalence app that provides up-to-date cases of Covid-19 around the world.It provides a dashboard that returns number of cases, deaths and recovered.The app also can give information about a country's status as well as travel advisories.Corona Tracker is responsive and can be viewed in different devices.
+                    </p>
+                </div>
+                <div className="project-links text-center p-2">
+                  <button className="btn btn-primary rounded-pill"><a href="https://github.com/iamtigermaximus/coronatracker2021" target="_blank" rel="noopener noreferrer" className="button-link"><FaLaptopCode/></a></button>
+                  <button className="btn btn-primary rounded-pill"><a href="https://tigercode-coronatracker2021.netlify.app/" target="_blank"  rel="noopener noreferrer" className="button-link"><FaGlobe/></a></button>
+                </div>
+              </div>
             </div>
+            <div className="col">
+              <div className="card h-100">
+                <img src={RestaurantImage} className="card-img-top" alt="..."/>
+                <div className="card-body">
+                  <h5 className="card-title text-center fw-bold">Restaurant Website- Moma</h5>
+                  <p className="card-text text-primary">Technologies Used:React, React Router, Bootstrap, HTML, CSS</p>
+                  <h6 card-title>The Brief</h6>
+                  <p className="card-text">
+                    A website gives a restaurant business a platform to provide the basic information the customers may need to know.It gives potential customers a feel for your restaurant, menu and atmosphere.
+                  </p>
+                  <h6 card-title>The Result</h6>
+                  <p className="card-text">
+                     Moma provides basic information about the restaurant's menu,the kind of service, how to book and reserve a table, contact information and a blog page.Moma is responsive and can be viewed in different devices. 
+                  </p>
+                </div>
+              <div className="project-links text-center p-2">
+                <button className="btn btn-primary rounded-pill"><a href="https://github.com/iamtigermaximus/restaurant-website" target="_blank" rel="noopener noreferrer" className="button-link"><FaLaptopCode/></a></button>
+                <button className="btn btn-primary rounded-pill"><a href="https://tigercode-restaurant-website.netlify.app/" target="_blank"  rel="noopener noreferrer" className="button-link"><FaGlobe/></a></button>
+              </div>
             </div>
-            </Fade>
-            <SocialMedia/>
-          
           </div>
+          <div className="col">
+            <div className="card h-100">
+              <img src={PortfolioImage} className="card-img-top" alt="..."/>
+              <div className="card-body">
+              <h5 className="card-title text-center fw-bold">Portfolio Website</h5>
+              <p className="card-text text-primary">Technologies Used:React, React Router, Bootstrap, HTML, CSS</p>
+              <h6 card-title>The Brief</h6>
+              <p className="card-text">
+                There are certain careers where portfolios prove more helpful than just resume in showcasing your skills and experience.As a self-taught developer, I believe that portfolio websites will help employers assess whether the skills and knowledge of a potential employee fits their needs.
+              </p>
+              <h6 card-title>The Result</h6>
+              <p className="card-text">
+               I built a portfolio website to showcase a collection of my personal projects.It also provides basic information about myself, contact details and links to project websites.The portfolio website is responsive and can be viewed in different devices.
+              </p>
+            </div>
+            <div className="project-links text-center p-2">
+              <button className="btn btn-primary rounded-pill"><a href="https://github.com/iamtigermaximus/2021portfolio2021" target="_blank" rel="noopener noreferrer" className="button-link"><FaLaptopCode/></a></button>
+              <button className="btn btn-primary rounded-pill"><a href="https://tigerportfolio2021.netlify.app/" target="_blank" rel="noopener noreferrer" className="button-link"><FaGlobe/></a></button>
+            </div>
+          </div>
+        </div>
+      </div
+    </div>
+  </div>
+  </Fade>
+        <SocialMedia />
+      </div>
     )
 }
 
