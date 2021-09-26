7a91bc9e7b0c82e90533fce7cd417aee535558f7
Siegfred <dante_sparda1925@yahoo.com>
2021-09-26 17:42:45 +0300

    fixed some bug

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -8,36 +8,35 @@ const SocialMedia = () => {
     return (
         <div className="social-media-container sticky-bottom pb-5">
             <div className="container">
-            <div className="container p-2 mx-5">
-                <div className="row">
-                    <div className="col-md-6 p-3">
-                        <div className="social-heading pb-2">
-                            <h3 className="social-title">Let's Work Together</h3>
-                        </div>
-                        <div className="social-subheading pb-2 mb-5">
+                <div className="container p-2 mx-5">
+                    <div className="row">
+                        <div className="col-md-6 p-3">
+                            <div className="social-heading pb-2">
+                                <h3 className="social-title">Let's Work Together</h3>
+                            </div>
+                            <div className="social-subheading pb-2 mb-5">
                             <h6 className="social-subtitle">Have a project you'd like to discuss?</h6>
                         </div>
-                        <button className="btn btn-primary rounded-pill mb-5"><Link className="button-link" to="/contact">Get in Touch</Link></button>
-                    </div>
-                    <div className="col-md-6 p-3 mb-5">
-                        <div className="social-media p-2">
-                            <div className="social-icon">
-                                <span><FaEnvelope/></span><a href="mailto:siegy.gamboa@gmail.com"  className="social-link">  SIEGFRED GAMBOA</a>
+                                <button className="btn btn-primary rounded-pill mb-5"><Link className="button-link" to="/contact">Get in Touch</Link></button>
                             </div>
-                            <div className="social-icon">
-                                <span><FaGithub/></span><a href="https://github.com/iamtigermaximus" className="social-link">  ON GITHUB</a>
-                            </div>
-                            <div className="social-icon">
-                                <FaInstagramSquare/><a href="mailto:dante_sparda1925@yahoo.com" className="social-link">  ON INSTAGRAM</a>
-                            </div>
-                           
-                            <div className="social-icon">
-                                <FaLinkedin/><a href="https://www.linkedin.com/in/siegfred-gamboa-5b1277218" className="social-link">  ON LINKEDIN</a>
+                        <div className="col-md-6 p-3 mb-5">
+                            <div className="social-media p-2">
+                                <div className="social-icon">
+                                    <span><FaEnvelope/></span><a href="mailto:siegy.gamboa@gmail.com"  className="social-link">  SIEGFRED GAMBOA</a>
+                                </div>
+                                <div className="social-icon">
+                                    <span><FaGithub/></span><a href="https://github.com/iamtigermaximus" className="social-link">  ON GITHUB</a>
+                                </div>
+                                <div className="social-icon">
+                                    <FaInstagramSquare/><a href="mailto:dante_sparda1925@yahoo.com" className="social-link">  ON INSTAGRAM</a>
+                                </div>
+                                <div className="social-icon">
+                                    <FaLinkedin/><a href="https://www.linkedin.com/in/siegfred-gamboa-5b1277218" className="social-link">  ON LINKEDIN</a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
             </div>
         </div>
     )
