52232b307de62b3249e6b59b4d64b905cc12046b
Siegfred <dante_sparda1925@yahoo.com>
2021-11-11 15:13:56 +0200

    aligned the codes for better readability

--- src/components/ProjectSlides.js
+++ src/components/ProjectSlides.js
@@ -6,45 +6,44 @@ const ProjectSlides = () => {
     return (
         <div className="project-slides-container pt-3">
             <div className="container text-center p-5">
-            <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
-            <div className="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-            <div className="carousel-inner project-slide-image">
-                <div className="carousel-item active ">
-                <img src={ProjectImage2}className="d-block w-100 "  alt="..."/>
-            <div className="carousel-caption d-none d-md-block">
-                <h5>PROJECT 1</h5>
-            </div>
-            </div>
-            <div className="carousel-item ">
-                <img src={ProjectImage2} className="d-block w-100" alt="..."/>
-            <div className="carousel-caption d-none d-md-block">
-                <h5>PROJECT 2</h5>
-            </div>
-            </div>
-            <div className="carousel-item">
-                <img src={ProjectImage2} className="d-block w-100" alt="..."/>
-            <div className="carousel-caption d-none d-md-block">
-                <h5>PROJECT 3</h5>
-            </div>
-            </div>
-            </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-            </button>
+                <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+                    <div className="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                <div className="carousel-inner project-slide-image">
+                    <div className="carousel-item active ">
+                        <img src={ProjectImage2}className="d-block w-100 "  alt="..."/>
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>PROJECT 1</h5>
+                        </div>
+                    </div>
+                    <div className="carousel-item ">
+                        <img src={ProjectImage2} className="d-block w-100" alt="..."/>
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>PROJECT 2</h5>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <img src={ProjectImage2} className="d-block w-100" alt="..."/>
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>PROJECT 3</h5>
+                        </div>
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
             </div>
             <button className="btn btn-primary rounded-pill mt-3"><Link className="button-link" to="/portfolio">View all Projects</Link></button>
-            </div>
-            
-        </div>
+        </div>    
+    </div>
     )
 }
 
